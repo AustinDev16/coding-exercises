5f6eaad18e4d0013b48f6c4b283294735697d3f0
Austin Blaser <austindev16@icloud.com>
2024-09-18 22:31:39 -0600

    Initialize project

---
+++ mobile/map-locations/ios/Locations/Locations/LocationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationsApp.swift
+//  Locations
+//
+//  Created by Austin Blaser on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct LocationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mobile/map-locations/ios/Locations/LocationsTests/LocationsTests.swift
@@ -0,0 +1,17 @@
+//
+//  LocationsTests.swift
+//  LocationsTests
+//
+//  Created by Austin Blaser on 9/18/24.
+//
+
+import Testing
+@testable import Locations
+
+struct LocationsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
